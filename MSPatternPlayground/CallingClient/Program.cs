c60d56801be1ea7a4cdf4d99653b82e9bec76605
hiepvo <k3c6.hiep@gmail.com>
2020-04-25 09:06:16 +0700

    add calling client

---
+++ MSPatternPlayground/CallingClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CallingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client start");
+            Run().Wait();
+           
+        }
+
+        static async Task Run()
+        {
+            
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+        }
+    }
+}
